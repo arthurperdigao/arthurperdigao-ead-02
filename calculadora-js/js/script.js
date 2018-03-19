bd30861565f0b74e3da6518033da38badbe74ed8
Arthur Perdigao <arthuradm2016@gmail.com>
2018-03-18 21:51:49 -0300

    feitinho

---
+++ calculadora-js/js/script.js
@@ -0,0 +1,20 @@
+﻿/**
+ * Exemplos de funções
+ */
+
+
+function calculadora() {
+	var valor1 = parseInt(prompt('Informe 1º valor: '));
+	var valor2 = parseInt(prompt('Informe 2º valor: '));
+	
+		alert('A + B= ' + (valor1 + valor2) + "\n"+
+			  'A - B= ' + (valor1 - valor2) + "\n"+
+			  'A * B= ' + (valor1 * valor2) + "\n"+
+			  'A / B= ' + (valor1 / valor2) + "\n"+
+			  'A % B= ' + (valor1 % valor2) + "\n");
+	
+}
+
+
+
+
